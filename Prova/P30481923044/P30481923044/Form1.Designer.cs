4aa07c5e7ebe160a7bd6b3354c7b27d3d5599462
HeitorLG <61881203+HeitorLG@users.noreply.github.com>
2020-06-19 22:20:20 -0300

    Add files via upload

---
+++ Prova/P30481923044/P30481923044/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace P30481923044
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxResultado = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbxResultado
+            // 
+            this.lstbxResultado.FormattingEnabled = true;
+            this.lstbxResultado.Location = new System.Drawing.Point(301, 47);
+            this.lstbxResultado.Name = "lstbxResultado";
+            this.lstbxResultado.Size = new System.Drawing.Size(460, 329);
+            this.lstbxResultado.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(82, 140);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(161, 107);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Verificar";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lstbxResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxResultado;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Prova/P30481923044/P30481923044/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace P30481923044
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double[,] vetor = new double[4, 4];
+            string valor = "";
+    
+            double resultadoMes = 0;
+            double totalFinal = 0;
+
+
+            for (int i = 0; i < vetor.GetLength(0); i++)
+            {
+                resultadoMes = 0;
+                for (int x = 0; x < vetor.GetLength(1); x++)
+                {
+                    valor = Interaction.InputBox("Digite valor das vendas da semana: " + (x + 1)
+                      , "Valores do mês " + (i + 1));
+                    if (valor == "")
+                        break;
+                    if (!double.TryParse(valor, out vetor[i, x]))
+                    {
+                        MessageBox.Show("Digite um valor numerico");
+                        x--;
+                    }
+                    resultadoMes = resultadoMes + vetor[i, x];
+                    lstbxResultado.Items.Add("Total da semana "+ (x+1) + " R$:" + valor + " do mês: " + (i+1));
+                    
+                }
+                totalFinal = totalFinal + resultadoMes;
+                lstbxResultado.Items.Add("Total do mês: " +(i+1)+ " R$: " +resultadoMes);
+            }
+            lstbxResultado.Items.Add("Total Geral: R$" + totalFinal);
+
+         
+        }
+    }
+    
+}
